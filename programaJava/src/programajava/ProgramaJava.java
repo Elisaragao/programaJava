a7a6c64608ba90da0546f64520960990160e06c5
Elisângela <elisangelaaragao644@gmail.com>
2024-09-30 11:41:10 -0300

    Add files via upload

---
+++ programaJava/src/programajava/ProgramaJava.java
@@ -0,0 +1,22 @@
+
+package programajava;
+
+public class ProgramaJava {
+
+    public static void main(String[] args) {
+       executarComando();
+       soma();
+    }
+    
+    public static void executarComando()
+    {
+        System.out.println("Hello, world!");
+    }
+    
+    public static void soma()
+    {
+    System.out.println (2000 + 24);
+    }
+}
+    
+    
